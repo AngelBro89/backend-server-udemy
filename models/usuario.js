69aa1640e876a99767cd385d67f812031edff98c
Angel <angeldominic1989@gmail.com>
2018-04-26 17:34:19 -0400

    Seccion 11 terminada - CRUD - Imagenes

--- models/usuario.js
+++ models/usuario.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-var uniqueValidator =require('mongoose-unique-validator');
+var uniqueValidator = require('mongoose-unique-validator');
 
 var Schema = mongoose.Schema;
 
@@ -11,16 +11,34 @@ var rolesValidos = {
 
 var usuarioSchema = new Schema({
 
-    nombre: { type: String, required: [true, 'El nombre es necesario'] },
-    email: { type: String, unique: true, required: [true, 'El correo es necesario'] },
-    password: { type: String, required: [true, 'La contraseña es necesaria.'] },
-    img: { type: String, required: false },
-    role: { type: String, required: true, default: 'USER_ROLE', enum: rolesValidos }
+    nombre: {
+        type: String,
+        required: [true, 'El nombre es necesario']
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: [true, 'El correo es necesario']
+    },
+    password: {
+        type: String,
+        required: [true, 'La contraseña es necesaria.']
+    },
+    img: {
+        type: String,
+        required: false
+    },
+    role: {
+        type: String,
+        required: true,
+        default: 'USER_ROLE',
+        enum: rolesValidos
+    }
 
 });
 
-usuarioSchema.plugin( uniqueValidator, { message: '{PATH} debe ser unico' } );
+usuarioSchema.plugin(uniqueValidator, {
+    message: '{PATH} debe ser unico'
+});
 
 module.exports = mongoose.model('Usuario', usuarioSchema);
-
-
